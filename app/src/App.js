45eaf40da26bda299a98864548fe90751e8c1ea9
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-17 10:12:12 +0100

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import ItemsList from './components/itemsList';
 
 import data from './data';
 
@@ -24,10 +25,11 @@ class App extends React.Component {
             <h1 className="store-header">Trinkets</h1>
           </Link>
           <div className="nav-links" />
-
+          <Link className="some-link" to="item-list">Shop</Link>
         </nav>
 
         <Route exact path="/" component={Home} />
+        <Route exact path="/item-list" component={ItemsList} />
       </div>
     );
   }
