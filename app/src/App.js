b3153747aba688a2bf35e0eeba47902ffb8f9ac6
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-11-10 20:45:08 +0000

    Convert App to Function Component

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
-import data from './data';
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ items: data });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <nav>
-          <h1 className="store-header">Trinkets</h1>
-          <div className="nav-links" />
-        </nav>
-      </div>
-    );
-  }
+function App() {
+	return (
+		<div className="App">
+			<nav>
+				<h1 className="store-header">Trinkets</h1>
+				<div className="nav-links" />
+			</nav>
+		</div>
+	);
 }
 
 export default App;
